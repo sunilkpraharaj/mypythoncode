4807c2df30985aeb585062737c4c901c2ee82fa4
Sunil Kumar Praharaj <sunil.praharaj@gmail.com>
2025-10-27 22:25:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import requests
 
 
-Json_Body = "{"NoOfConsumer": 10,"MessageType": "inv-inv-ViewItemCacheRebuild","MinNoOfConsumer": 1,"MaxDesiredInstances": 6}"
-
+Json_Body = {
+    "NoOfConsumer": 10,
+    "MessageType": "inv-inv-ViewItemCacheRebuild",
+    "MinNoOfConsumer": 1,
+    "MaxDesiredInstances": 6
+}
 json_object = json.loads(new_component_property)
 print("Logged in successfully ->> Test1!")
 print("Hello from GitHub! Sunil")
