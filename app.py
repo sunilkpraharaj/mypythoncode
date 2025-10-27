405844977a47a4a2f044678b4b5af63899f0e0b7
Sunil Kumar Praharaj <sunil.praharaj@gmail.com>
2025-10-27 22:42:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ Json_Body = {
     "MinNoOfConsumer": 1,
     "MaxDesiredInstances": 6
 }
-json_object = json.dumps(Json_Body)
+json_string = json.dumps(Json_Body)
+print(json_string)
 print("Logged in successfully ->> Test1!")
 print("Hello from GitHub! Sunil")
