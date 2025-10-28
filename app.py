1ca5e83aac3b4bd424744cefb878a5d641f7db6a
Sunil Kumar Praharaj <sunil.praharaj@gmail.com>
2025-10-28 08:37:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,15 @@ import json
 
 
 Json_Body = {
-    "NoOfConsumer": 10,
-    "MessageType": "inv-inv-ViewItemCacheRebuild",
-    "MinNoOfConsumer": 1,
-    "MaxDesiredInstances": 6,
-    "OrderLinePromisingInfo": 
-        {
-            "LastPossibleDeliveryDate": null,
-            "IsDeliveryByRequestedDate": false,
-            "ExternalRouteId": "ecomorg:T0981"
-        }
+  "NoOfConsumer": 10,
+  "MessageType": "inv-inv-ViewItemCacheRebuild",
+  "MinNoOfConsumer": 1,
+  "MaxDesiredInstances": 6,
+  "OrderLinePromisingInfo": {
+    "LastPossibleDeliveryDate": null,
+    "IsDeliveryByRequestedDate": false,
+    "ExternalRouteId": "ecomorg:T0981"
+  }
 }
 json_string = json.dumps(Json_Body)
 response = json.loads(json_string)
