8361eada021bc2d7a6ac8663980913cfa5420f8d
Sunil Kumar Praharaj <sunil.praharaj@gmail.com>
2025-10-28 09:01:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ Json_String = '''{
 }'''
 Json_Body = json.loads(Json_String)
 print("Consumer Count -> ",Json_Body.get("NoOfConsumer"))
-print(Json_Body["OrderLinePromisingInfo"]["ExternalRouteId"])
-print("OrderLine Promising Info -> ",Json_Body.get("OrderLinePromisingInfo"),{}).get("ExternalRouteId")
+print("OrderLine Promising Info -> ", Json_Body["OrderLinePromisingInfo"]["ExternalRouteId"])
 print("Logged in successfully ->> Test1!")
 print("Hello from GitHub! Sunil")
