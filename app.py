c668442a22d96cbe667758ec7ea7f179aec7916d
Sunil Kumar Praharaj <sunil.praharaj@gmail.com>
2025-10-28 14:01:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,54 @@ Json_String = '''{
                     "State": "TX"
                 }
             }
+        },
+        {
+            "OrderLineId": "2",
+            "IsOnHold": false,
+            "ServiceLevelCode": "UPS Express",
+            "CarrierCode": "UPS",
+            "ShippingMethodId": null,
+            "IsCancelled": false,
+            "ItemId": "Laptop2",
+            "UnitPrice": "5",
+            "Quantity": "1",
+            "IsReturnable": true,
+            "UOM": "EA",
+            "ShipToLocationId": null,
+            "RequestedDeliveryDate": "2023-05-15T10:42:22.664",
+            "OrderLinePromisingInfo": {
+                "LastPossibleDeliveryDate": null,
+                "IsDeliveryByRequestedDate": false,
+                "ExternalRouteId": "ecomorg:T0981"
+            },
+            "OrderLineNote": [
+                {
+                    "NoteType": "Gift From",
+                    "NoteCode": "INS",
+                    "NoteText": "Instruction - Line",
+                    "NoteId": "2",
+                    "NoteCategory": "Instruction"
+                }
+            ],
+            "DeliveryMethod": {
+                "DeliveryMethodId": "ShipToAddress"
+            },
+            "ShipToAddress": {
+                "AddressTypeId": "Shipping",
+                "IsAddressVerified": true,
+                "Address": {
+                    "Address1": "14131 Midway Road",
+                    "Address2": "",
+                    "City": "Addison",
+                    "Country": "US",
+                    "Email": "jSmith@manh.com",
+                    "FirstName": "John",
+                    "LastName": "Smith",
+                    "Phone": "423-306-1973",
+                    "PostalCode": "30039",
+                    "State": "TX"
+                }
+            }
         }
     ]
 }'''
